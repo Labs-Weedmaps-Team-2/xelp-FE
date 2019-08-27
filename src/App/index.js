df5dadaa27f998e2784c8a822108ca88aab7fe1f
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-08-27 15:01:56 -0700

    setups cssReset, react-router, redux

---
+++ src/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CSSReset from '../styles/cssReset'
+
+function App() {
+  return (
+    <div>
+      <CSSReset />
+      <div>App</div>
+    </div>
+  )
+}
+
+export default App
