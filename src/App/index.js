54cc88ba71ac4c2875dfa6f2efdad9b3d28a2de9
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-08-27 15:10:08 -0700

    setups up routes

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,20 @@
-import React from 'react'
-import CSSReset from '../styles/cssReset'
+import React, { lazy, Suspense } from 'react'
+import styled from 'styled-components'
+import { Route, Switch } from 'react-router-dom'
 
-function App() {
+import CSSReset from '../styles/cssReset'
+const Home = lazy(() => import('../pages/Home'))
+
+const App = () => {
   return (
     <div>
       <CSSReset />
-      <div>App</div>
+      <nav>App</nav>
+      <Suspense fallback={null}>
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </Suspense>
     </div>
   )
 }
