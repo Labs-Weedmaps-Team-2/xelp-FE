8cb2f51a19d93f326ac7a5f1fbf6dff6984518a2
luis <mrluismartinezzz@gmail.com>
2019-08-29 20:13:52 -0700

    created SignIn component

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <div>
+      <div>SignIn with Facebook</div>
+      <div>SignIn with Google</div>
+      <div>SignIn with Github</div>
+    </div>
+  )
+}
+
+export default SignIn
