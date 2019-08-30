9c2d8a4c96d50c26fabd90e42e2ffd5c1d349414
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-08-30 09:39:23 -0700

    refactors sign-in presentation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-import SignIn from 'pages/SignIn'
 
 const Navbar = () => {
   return (
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,24 +1,68 @@
 import React from 'react'
+import GoogleSvg from 'assets/svg/google.svg'
+import FacebookSvg from 'assets/svg/facebook.svg'
+import GithubSvg from 'assets/svg/github.svg'
 import styled from 'styled-components'
-import SignInButtons from './createButtons.js'
+import { authUrl } from 'config'
+
+const authProviders = [
+  { name: 'Google', logo: GoogleSvg },
+  { name: 'Facebook', logo: FacebookSvg },
+  { name: 'Github', logo: GithubSvg },
+]
+
+const createProviderLink = provider => {
+  return provider.name.toLowerCase() === 'google'
+    ? 'google_oauth2'
+    : provider.name.toLowerCase()
+}
+
 const SignIn = () => {
-  return <SignInWrapper>{SignInButtons}</SignInWrapper>
+  return (
+    <StyledSignIn>
+      {authProviders.map(provider => (
+        <a
+          key={provider.name}
+          href={`${authUrl}/${createProviderLink(provider)}`}
+        >
+          <button>
+            <img src={provider.logo} alt={`${provider.name} sign in`} />
+            <span>{`Sign in with ${provider.name}`} </span>
+          </button>
+        </a>
+      ))}
+    </StyledSignIn>
+  )
 }
 
 export default SignIn
 
-const SignInWrapper = styled.div`
-  border: 2px solid blue;
-  width: 800px;
-
+const StyledSignIn = styled.div`
+  /* border: 2px solid blue; */
+  max-width: 320px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
   a {
-    border: 2px solid red;
-    width: 800px;
+    /* border: 2px solid red; */
+    display: flex;
+    justify-content: center;
+    margin: 10px 0;
   }
   button {
-    padding: 1rem 1.5rem;
-  }
-  img {
-    max-width: 55px;
+    display: flex;
+    width: 100%;
+    align-items: center;
+    padding: 1.5rem;
+    cursor: pointer;
+    background-color: white;
+    span {
+      padding-left: 30px;
+      font-size: 2rem;
+    }
+    img {
+      max-width: 50px;
+    }
   }
 `
