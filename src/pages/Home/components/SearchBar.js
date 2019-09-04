476b138060cddbf29e17d08a5615c493a5877bd2
Brad Mortensen <b.mort86@gmail.com>
2019-09-04 16:42:10 -0600

    Adds hero image to search background div

--- src/pages/Home/components/SearchBar.js
+++ src/pages/Home/components/SearchBar.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+
 
 const SearchBar = () => {
   return (
-    <div>
+    <StyledHero >
       SearchBar
-    </div>
+    </StyledHero>
   )
 }
 
 export default SearchBar
+
+const StyledHero = styled.div`
+  background-image: url("https://s3-media3.fl.yelpcdn.com/assets/srv0/yelp_large_assets/fa674c810afa/assets/img/home/hero_photos/sL6pI2T8M-jSGiZzeq6Wxg.jpg");
+  background-color: #333;
+  background-size: cover;
+  background-position: 50%;
+  height: 570px;
+`
