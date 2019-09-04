361dee798672cc2cdf257200e8161dc9fcf846eb
Brad Mortensen <b.mort86@gmail.com>
2019-09-04 16:53:29 -0600

    adds inputs to splash page

--- src/pages/Home/components/SearchBar.js
+++ src/pages/Home/components/SearchBar.js
@@ -5,7 +5,13 @@ import styled from 'styled-components'
 const SearchBar = () => {
   return (
     <StyledHero >
-      SearchBar
+      <div className="inputs-container">
+        <div className="item-search-container"></div>
+        <div className="locale-search-container"></div>
+        <div className="search-button">
+          GO!
+        </div>
+      </div>
     </StyledHero>
   )
 }
@@ -18,4 +24,29 @@ const StyledHero = styled.div`
   background-size: cover;
   background-position: 50%;
   height: 570px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .inputs-container {
+    width: 80%;
+    height: 50px;
+    background-color: white;
+    border-radius: 5px;
+    display: flex;
+    flex-flow: row;
+    .item-search-container {
+      width:47%;
+    }
+    .locale-search-container {
+      width:47%;
+    }
+    .search-button {
+      width: 6%;
+      background-color: red;
+      color: white;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
 `
