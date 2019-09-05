bfe36e14742cd33b592638b72cb18920161a3739
Brad Mortensen <b.mort86@gmail.com>
2019-09-05 16:32:05 -0600

    remove use state import

--- src/pages/Home/components/SearchBar.js
+++ src/pages/Home/components/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect} from 'react'
 import styled from 'styled-components'
 import { useRouter, usePosition } from 'hooks'
 import { useDispatch, useSelector } from 'react-redux'
