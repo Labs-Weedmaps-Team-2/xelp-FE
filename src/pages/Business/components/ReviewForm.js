6b954629c5cc2e1dd1d6890af38f4462ad4bc118
luis <mrluismartinezzz@gmail.com>
2019-09-05 14:12:56 -0700

    form has radios for rating value

--- src/pages/Business/components/ReviewForm.js
+++ src/pages/Business/components/ReviewForm.js
@@ -2,18 +2,20 @@ import React, { useState } from 'react'
 import { api } from 'apis'
 import { useDispatch } from 'react-redux'
 import useRouter from 'hooks/useRouter'
+
 const ReviewForm = () => {
   const dispatch = useDispatch()
   const { history } = useRouter()
 
   const [reviewText, setReviewText] = useState('')
+  const [rateValue, setRateValue] = useState(null)
   const handleSubmit = e => {
     e.preventDefault()
     api
       .post(`/review/${window.location.pathname.split('/writeareview/')[1]}`, {
         review: {
           text: reviewText,
-          rating: 3,
+          rating: rateValue,
         },
       })
       .then(res => {
@@ -28,10 +30,47 @@ const ReviewForm = () => {
   const handleInput = event => {
     setReviewText(event.target.value)
   }
+  const handleClick = e => {
+    setRateValue(Number(e.target.value))
+  }
   return (
-    <form onSubmit={e => handleSubmit(e)}>
+    <form
+      style={{ display: 'flex', flexDirection: 'column' }}
+      onSubmit={e => handleSubmit(e)}
+    >
       <label>Review</label>
-      <input onChange={e => handleInput(e)} value={reviewText} />
+      <input
+        type='radio'
+        name='rating'
+        value='1'
+        onClick={e => handleClick(e)}
+      />
+      <input
+        type='radio'
+        name='rating'
+        value='2'
+        onClick={e => handleClick(e)}
+      />
+      <input
+        type='radio'
+        name='rating'
+        value='3'
+        onClick={e => handleClick(e)}
+      />
+      <input
+        type='radio'
+        name='rating'
+        value='4'
+        onClick={e => handleClick(e)}
+      />
+      <input
+        type='radio'
+        name='rating'
+        value='5'
+        onClick={e => handleClick(e)}
+      />
+      <textarea onChange={e => handleInput(e)} value={reviewText} />
+      <button type='submit' />
     </form>
   )
 }
