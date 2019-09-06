4050371a7c633526cc730eb4b44b40a0b51f40df
Holly Giang <holly.giang@gmail.com>
2019-09-06 05:11:26 -0400

    Add logo to homepage

--- src/pages/Home/components/SearchBar.js
+++ src/pages/Home/components/SearchBar.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import usePosition from 'hooks/usePosition'
 
-
 const SearchBar = () => {
   const { latitude, longitude } = usePosition();
   const [locale, setLocale] = useState('')
@@ -16,6 +15,7 @@ const SearchBar = () => {
   }, [latitude, longitude])
   return (
     <StyledHero >
+      <StyledLogo src="https://s3-media1.fl.yelpcdn.com/assets/srv0/styleguide/891ac3707136/assets/img/brand_guidelines/yelp_fullcolor@2x.png" alt="Logo" />;
       <div className="inputs-container">
         <div className="search-container type">
           <p>What?</p>
@@ -95,3 +95,14 @@ const StyledHero = styled.div`
     }
   }
 `
+
+const StyledLogo = styled.img`
+  position: absolute;
+  top: 15%;
+  width: 200px;
+  height: auto;
+  padding: 0;
+  margin: 0 auto 48px;
+  background-repeat: no-repeat;
+}
+`;
