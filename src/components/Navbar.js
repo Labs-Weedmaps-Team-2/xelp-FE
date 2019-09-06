ad8155ee97413ccc29869db01ced413ec999c9cc
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-09-06 09:54:31 -0700

    minor navlink tweaks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+import { useRouter } from 'hooks'
 
 const StyledLink = styled(Link)`
-  color: white;
   font-weight: bolder;
-`;
+`
 
 export const Navbar = () => {
+  const { location } = useRouter()
   return (
-    <StyledNavHeader>
+    <StyledNavHeader location={location}>
       <StyledLink to='/'>Home</StyledLink>
       <StyledLink to='/sign-in'>Sign In</StyledLink>
       <StyledLink to='/profile'>Profile</StyledLink>
@@ -20,9 +21,9 @@ export const Navbar = () => {
 
 const StyledNavHeader = styled.nav`
   background: transparent;
-  position:fixed;
-  left:0;
-  top:0;
+  position: fixed;
+  left: 0;
+  top: 0;
   max-width: 1010px;
   padding: 26px 20px;
   width: 100%;
@@ -30,12 +31,15 @@ const StyledNavHeader = styled.nav`
   align-items: center;
   justify-content: center;
   height: 60px;
+
   a {
+    color: ${props =>
+      props.location.pathname.includes('profile') ? 'black' : 'white'};
     font-size: 1.8rem;
     margin-left: 5rem;
-}
+  }
 `
 const NavRight = styled.div`
   width: 33.333%;
   text-align: right;
-`;
+`
