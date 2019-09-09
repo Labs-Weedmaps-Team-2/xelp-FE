5f8c7cab3780716cfaf49db8711eafa33a64add0
luis <mrluismartinezzz@gmail.com>
2019-09-09 12:03:28 -0700

    skeletion code for basic business form

--- src/pages/Business/components/AddBusinessForm.js
+++ src/pages/Business/components/AddBusinessForm.js
@@ -12,9 +12,18 @@ const BusinessForm = () => {
       }}
     >
       <label>Add Business</label>
-      <input placeholder='business name' />
-      <input placeholder='address ex' />
-      <input placeholder='hours' />
+      <div>
+        <label>Required Information</label>
+        <input placeholder='business name' />
+        <input placeholder='123 Main St' />
+        <input placeholder='San Francisco, CA 94103' />
+      </div>
+      <div>
+        <label>Optional Details</label>
+        <textarea placeholder='hours' />
+        <input placeholder='phone #' />
+        <input placeholder='Website' />
+      </div>
     </FormWrapper>
   )
 }
@@ -22,7 +31,22 @@ const BusinessForm = () => {
 export default BusinessForm
 
 const FormWrapper = styled.form`
+  max-width: 400px;
   input {
     border: 2px solid blue;
+    margin-bottom: 0.5rem;
+  }
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+  label {
+    text-align: center;
+  }
+  textarea {
+    resize: none;
+    height: 60px;
+    border: 2px solid goldenrod;
+    margin-bottom: 0.5rem;
   }
 `
--- src/pages/Business/index.js
+++ src/pages/Business/index.js
@@ -22,7 +22,6 @@ const Business = () => {
     dispatch(fetchBusinessDetails(yelp_id[2]))
     return () => {
       dispatch(resetSingleBusiness())
-      renderRating(business.rating)
     }
   }, [])
 
