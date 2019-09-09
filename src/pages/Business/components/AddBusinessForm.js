1cb9a1c6c7c8f0a8b53ac150c9bd951d0b89d974
luis <mrluismartinezzz@gmail.com>
2019-09-09 12:19:04 -0700

    controlled form input values

--- src/pages/Business/components/AddBusinessForm.js
+++ src/pages/Business/components/AddBusinessForm.js
@@ -2,6 +2,21 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import { useDispatch } from 'react-redux'
 const BusinessForm = () => {
+  const [form, setValues] = useState({
+    businessName: '',
+    businessAddress: '',
+    businessState: '',
+    businessHours: '',
+    businessPhoneNum: '',
+    businessWebsite: '',
+  })
+  const updateField = e => {
+    setValues({
+      ...form,
+      [e.target.name]: e.target.value,
+    })
+  }
+
   return (
     <FormWrapper
       style={{
@@ -14,15 +29,45 @@ const BusinessForm = () => {
       <label>Add Business</label>
       <div>
         <label>Required Information</label>
-        <input placeholder='business name' />
-        <input placeholder='123 Main St' />
-        <input placeholder='San Francisco, CA 94103' />
+        <input
+          placeholder='business name'
+          value={form.businessName}
+          onChange={updateField}
+          name='businessName'
+        />
+        <input
+          placeholder='123 Main St'
+          value={form.businessAddress}
+          onChange={updateField}
+          name='businessAddress'
+        />
+        <input
+          placeholder='San Francisco, CA 94103'
+          value={form.businessState}
+          onChange={updateField}
+          name='businessState'
+        />
       </div>
       <div>
         <label>Optional Details</label>
-        <textarea placeholder='hours' />
-        <input placeholder='phone #' />
-        <input placeholder='Website' />
+        <textarea
+          placeholder='hours'
+          value={form.businessHours}
+          onChange={updateField}
+          name='businessHours'
+        />
+        <input
+          placeholder='phone #'
+          value={form.businessPhoneNum}
+          onChange={updateField}
+          name='businessPhoneNum'
+        />
+        <input
+          placeholder='Website'
+          value={form.businessWebsite}
+          onChange={updateField}
+          name='businessWebsite'
+        />
       </div>
     </FormWrapper>
   )
