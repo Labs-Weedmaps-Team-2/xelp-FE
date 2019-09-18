115c45b7c14050b49f1d620545ac75b58355be9f
Brad Mortensen <b.mort86@gmail.com>
2019-09-17 20:01:13 -0600

    styles logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -8,11 +8,11 @@ import blackLogo from '../assets/img/blacklogo.png'
 export const Logo = () => {
   return (
     <Link to='/'>
-      <StyledLogo className="nightlyfe"src={blackLogo} alt="nightlyfe logo"/>
+      <StyledLogo src={blackLogo} alt="nightlyfe logo"/>
     </Link>
   )
 }
 
 const StyledLogo = styled.img`
-  width: 150px;
+  width: 165px;
 `
