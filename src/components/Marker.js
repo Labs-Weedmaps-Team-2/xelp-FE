a9ca884f67f95bfd8f25e9998b0d0dc9e16c40e4
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-09-20 01:21:19 -0700

    removes unused packages; adds popup arrow

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -15,6 +15,7 @@ export const Marker = ({ $hover, color, item, id, listHover }) => {
       <span>{id}</span>
       <MarkerSvg className='marker-svg' color={hoverColor} />
       <div className='hover-popup'>
+        <div className='arrow-up'></div>
         <div
           style={{ width: 120, marginLeft: 8, marginTop: 5, paddingBottom: 10 }}
         >
@@ -80,7 +81,7 @@ const StyledMarker = styled.div`
     font-weight: bold;
     font-size: 10px;
     color: white;
-    top: 5px;
+    top: 6px;
   }
   .marker-svg {
     color: red;
@@ -92,11 +93,22 @@ const StyledMarker = styled.div`
     justify-content: space-between;
     min-height: 60px;
     min-width: 200px;
-    top: -60px;
-    left: -40px;
+    top: -50px;
+    left: -65px;
     background: white;
     transition: all 0.3s ease;
     border-radius: 8px;
     border: 1px solid rgb(207, 207, 205);
+    .arrow-up {
+      position: absolute;
+      bottom: -8px;
+      right: 12.5%;
+      width: 0;
+      height: 0;
+      z-index: 1000;
+      border-left: 9px solid transparent;
+      border-right: 9px solid transparent;
+      border-top: 9px solid white;
+    }
   }
 `
